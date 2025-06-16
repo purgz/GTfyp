94aeb6cbe2dd0e16a1b169ccbf2f193d136aeb2c
Henry Brooks <henbrooks6@gmail.com>
2025-06-16 15:36:27 +0100

    generalized local process

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from source import simulation
 import argparse
 
 import pandas as pd
+import numpy as np
 
 """
 Main entry point for using standalone simulation 
@@ -11,15 +12,27 @@ Main entry point for using standalone simulation
 Hav cmd line interface to run simulations and plot results.
 """
 
+pdArray = np.array([[3, 0],
+           [5, 1]])
+
+
 # Need this because of multiprocessing - idk why
 if  __name__ == "__main__":
 
-    mResults, lResults = simulation.runSimulationPool()
+    mResults, lResults = simulation.runSimulationPool(matrix=pdArray, popSize=100, simulations=1, initialDist=[0.5,0.5], iterations=100000, w=0.4)
 
 
+    """ 
     df_RPS_MO = pd.DataFrame({"c1": mResults[0], "c2": mResults[1], "c3": mResults[2], "c4": mResults[3]})
 
     df_RPS_LU = pd.DataFrame({"c1": lResults[0], "c2": lResults[1], "c3": lResults[2], "c4": lResults[3]})
+    """
+
+    df_RPS_MO = pd.DataFrame({"c1": mResults[0], "c2": mResults[1]})
+
+    df_RPS_LU = pd.DataFrame({"c1": lResults[0], "c2": lResults[1]})
+
+
 
 
     print(df_RPS_LU.tail())
