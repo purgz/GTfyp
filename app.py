90f7a97a433cf97e8da3d728978393c48ee5c63c
Henry Brooks <henbrooks6@gmail.com>
2025-06-17 13:24:03 +0100

    testing different population sizes + global variable bug fix

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import argparse
 import pandas as pd
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 """
 Main entry point for using standalone simulation 
 
@@ -20,9 +22,26 @@ pdArray = np.array([[3, 0],
 if  __name__ == "__main__":
 
     #mResults, lResults = simulation.runSimulationPool(matrix=pdArray, popSize=100, simulations=1, initialDist=[0.5,0.5], iterations=100000, w=0.4, H=1)
+    deltaM = []
+    deltaL = []
 
-    mResults, lResults = simulation.runSimulationPool(popSize=2000,simulations=1,H=3, initialDist=[0.2,0.2, 0.6, 0], w=0.3, iterations = 100000)
-    
+    mResults, lResults, deltaMoran, deltaLocal = simulation.runSimulationPool(popSize=100,simulations=100,H=3, initialDist=[0.25,0.25, 0.25, 0.25], w=0.3, iterations = 100000)
+    deltaM.append(deltaMoran)
+    deltaL.append(deltaLocal)
+    mResults, lResults, deltaMoran, deltaLocal = simulation.runSimulationPool(popSize=120,simulations=100,H=3, initialDist=[0.25,0.25, 0.25, 0.25], w=0.3, iterations = 100000)
+    deltaM.append(deltaMoran)
+    deltaL.append(deltaLocal)
+    mResults, lResults, deltaMoran, deltaLocal = simulation.runSimulationPool(popSize=300,simulations=100,H=3, initialDist=[0.25,0.25, 0.25, 0.25], w=0.3, iterations = 100000)
+    deltaM.append(deltaMoran)
+    deltaL.append(deltaLocal)
+    mResults, lResults, deltaMoran, deltaLocal = simulation.runSimulationPool(popSize=500,simulations=100,H=3, initialDist=[0.25,0.25, 0.25, 0.25], w=0.3, iterations = 100000)
+    deltaM.append(deltaMoran)
+    deltaL.append(deltaLocal)
+    mResults, lResults, deltaMoran, deltaLocal = simulation.runSimulationPool(popSize=1000,simulations=100,H=3, initialDist=[0.25,0.25, 0.25, 0.25], w=0.3, iterations = 100000)
+    deltaM.append(deltaMoran)
+    deltaL.append(deltaLocal)
+
+
     df_RPS_MO = pd.DataFrame({"c1": mResults[0], "c2": mResults[1], "c3": mResults[2], "c4": mResults[3]})
 
     df_RPS_LU = pd.DataFrame({"c1": lResults[0], "c2": lResults[1], "c3": lResults[2], "c4": lResults[3]})
@@ -34,6 +53,13 @@ if  __name__ == "__main__":
     df_RPS_LU = pd.DataFrame({"C": lResults[0], "D": lResults[1]})
     """
 
+
+    plt.plot(deltaM, label="moran")
+    plt.plot(deltaL, label="local")
+
+    plt.legend()
+
+    plt.show()
 
 
     print(df_RPS_LU.tail())
