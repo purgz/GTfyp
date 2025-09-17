cb953f0b9674448b90a0e3d5a11b37d1c2ac25ea
Henry Brooks <henbrooks6@gmail.com>
2025-09-17 15:18:07 +0100

    work on plotly in the frontend - need to decide whether to just send data and process in frontend or build graphs in backend then send - need to think of a good structure for the api

--- source/backend/urls.py
+++ source/backend/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from .views import HelloWorldView
 from .views import SimpleTemplateView
+from .views import SimulationDataView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/hello/', HelloWorldView.as_view(), name='hello_world'),
+    path('api/simulationTest/', SimulationDataView.as_view(), name='simulation_test'),
     path('landing/', SimpleTemplateView.as_view(), name='landing_page'),
 ]
