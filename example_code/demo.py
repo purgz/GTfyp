d90464cd93aa00780b4ddac6a5db6feea1ccdb25
Henry Brooks <henbrooks6@gmail.com>
2026-04-26 20:05:38 +0100

    .

---
+++ example_code/demo.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+import simulation
+
+import replicator
+
+import pandas as pd
+
+from simulation.games import Games
+
+
+
+def main():
+    print("Sucessful import")
+
+
+if __name__ == "__main__":
+    main()
